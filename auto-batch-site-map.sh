8009848d510da1540c0f0d1ef0f6712e5582f69a
Ivan Barbosa Pinheiro <ivanpinheiro@outlook.com>
2024-04-17 16:42:47 -0300

    update shell script

--- auto-batch-site-map.sh
+++ auto-batch-site-map.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
-projectName="projeto_uam"
+
+# Armazena o nome do arquivo fornecido
+file="./project_input/project_name.txt"
+
+# Verifica se o arquivo existe
+if [ ! -f "$file" ]; then
+    echo "Erro ao ler o projeto."
+    exit 1
+fi
+
+# Lê a primeira linha do arquivo e armazena em uma variável
+projectName=$(head -n 1 "$file")
+
+# Define o caminho da pasta reports
+reportsFolder="./project_output/$projectName/reports"
+
+# Verifica se a pasta reports não existe
+if [ ! -d "$reportsFolder" ]; then
+    # Cria a pasta reports
+    mkdir -p "$reportsFolder"
+fi
+
 siteMapFolder="./project_input/"$projectName"/site_map"
 
 if [ ! -d "$siteMapFolder" ]; then
@@ -9,7 +30,7 @@ fi
 
 siteMapFiles=$(find "$siteMapFolder" -type f -name "*.xml")
 
-for arquivo in $siteMapFiles; do
-  fileName=$(basename "$arquivo" .xml)
-  npm run cli -- -np -p "$arquivo" -d ./project_output/"$projectName"/reports/"$fileName"/
+for file in $siteMapFiles; do
+  fileName=$(basename "$file" .xml)
+  npm run cli -- -np -p "$file" -d ./project_output/"$projectName"/reports/"$fileName"/
 done
--- start.sh
+++ start.sh
@@ -4,16 +4,28 @@
 echo "Digite o nome do projeto, por exemplo, 'projeto_hagens':"
 read name
 
-# Cria a pasta com o nome fornecido pelo usuário
-mkdir ./project_input
-mkdir ./project_output
-mkdir ./project_input/"$name"
-mkdir ./project_input/"$name"/site_map
+# Cria as pastas necessárias para o projeto
+mkdir -p ./project_input/"$name"/site_map
+mkdir -p ./project_output/"$name"
+
+# Nome do arquivo
+file="./project_input/project_name.txt"
+
+# Cria o arquivo e escreve o nome do projeto nele
+echo "$name" > "$file"
+
+# Verifica se o arquivo foi criado com sucesso
+if [ $? -eq 0 ]; then
+    echo "O projeto '$name' foi criado com sucesso!"
+else
+    echo "Erro ao criar o projeto '$name'."
+    exit 1
+fi
 
 # Verifica se a pasta foi criada com sucesso
-if [ $? -eq 0 ]; then
+if [ -d "./project_input/$name" ]; then
     echo "Pasta '$name' criada com sucesso!"
-    echo "Adicione os arquivos de site map em './project_input/"$name"/site_map'"
+    echo "Adicione os arquivos de site map em './project_input/$name/site_map'"
 else
-    echo "Erro ao criar a pasta '$nome'."
+    echo "Erro ao criar a pasta '$name'."
 fi
